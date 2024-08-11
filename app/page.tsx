9eff3a5e02891f2796473b8399385a533c5c148a
Teddy1404 <aryansinghchandel1404@gmail.com>
2024-08-11 21:52:16 +0530

    authentacation setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+   <div>
+    <h1>hello </h1>
+    <Button> functions</Button>
+   </div>
+  );
+}
