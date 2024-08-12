1edd4bc4fac527088ca05131f44c660a5b93caa5
Teddy1404 <aryansinghchandel1404@gmail.com>
2024-08-12 22:35:31 +0530

    handled hooks for searchsection

--- app/dashboard/_components/SearchSection.tsx
+++ app/dashboard/_components/SearchSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Search } from 'lucide-react'
-function SearchSection() {
+import { eventNames } from 'process'
+function SearchSection({onSearchInput}:any) {
   return (
     <div className='p-10 bg-gradient-to-br from-purple-500 via-purple-700 to-blue-700 justify-center items-center flex flex-col text-white'>
         <h2 className='text-3xl font-bold'>Browse all templates</h2>
@@ -8,7 +9,9 @@ function SearchSection() {
         <div className='w-full flex justify-center' >
             <div className='flex gap-2 items-center p-2 border rounded-md bg-white my-5 w-[50%]'>
                 <Search  className='text-primary' />
-                <input type='text' placeholder='Search' className='bg-transparent w-full outline-none text-black'/>
+                <input type='text' placeholder='Search' className='bg-transparent w-full outline-none text-black'
+                onChange={(event)=>onSearchInput(event.target.value)}
+                />
             </div>
         </div>
     </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import SearchSection from './_components/SearchSection'
 import { TemplateContext } from 'next/dist/shared/lib/app-router-context.shared-runtime'
 import TemplateList from './_components/TemplateList'
 
 const Dashboard = () => {
+  const [userSearchInput,setUserSearchInput] = useState<string>();
   return (
     <div>
       
       {/* search section */}
-      <SearchSection/>
+      <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
       {/* template list */}
-      <TemplateList/>
+      <TemplateList userSearchInput={userSearchInput}/>
     </div>
   )
 }
