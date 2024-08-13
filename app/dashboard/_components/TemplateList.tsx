d642ecd87e66b78e49c3a3af244384c117382280
Teddy1404 <aryansinghchandel1404@gmail.com>
2024-08-13 11:49:42 +0530

    filter of template cards done

--- app/dashboard/_components/TemplateList.tsx
+++ app/dashboard/_components/TemplateList.tsx
@@ -1,5 +1,5 @@
 import Templates from '@/app/(data)/Templates'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import TemplateCard from './TemplateCard'
 
 export interface TEMPLATE{
@@ -20,12 +20,20 @@ export interface FORM{
 
 function TemplateList({userSearchInput}:any) {
 
+    const [templatesList, setTemplatesList] =useState(Templates)
+
     useEffect(()=>{
-        console.log(userSearchInput)
+       if(userSearchInput){
+        const filterData = Templates.filter(item=>item.name.toLowerCase().includes(userSearchInput.toLowerCase()));
+        setTemplatesList(filterData);
+       }
+       else{
+        setTemplatesList(Templates);
+       }
     },[userSearchInput])
   return (
     <div className='grid grid-cols-2 md:grid-col-3 lg:grid-cols-4 gap-5 p-10'>
-        {Templates.map((item:TEMPLATE,index:number)=>(
+        {templatesList.map((item:TEMPLATE,index:number)=>(
            <TemplateCard key={index}{...item}/>
         ))}
     </div>
