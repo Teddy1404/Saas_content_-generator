aaf9386c916255302c4768f6ea031c7ce4768d4d
Teddy1404 <aryansinghchandel1404@gmail.com>
2024-08-13 17:43:51 +0530

    ouput section editor implementatation

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css';
 
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 function OutputSection() {
+  const editorRef:any = useRef();
   return (
-    <div>OutputSection</div>
+    <div className='bg-white shadow-lg border'>
+      
+    <div className='flex justify-between items-center p-5'>
+      <h2>Your result</h2>
+      <Button><Copy/>copy</Button>
+    </div>
+    <Editor
+    ref={editorRef}
+    initialValue="hello react editor world!"
+    height="600px"
+    initialEditType="wysiwyg"
+    useCommandShortcut={true}
+    onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+  />
+    </div>
   )
 }
 
